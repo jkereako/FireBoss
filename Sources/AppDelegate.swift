164542550ac4d80798f616704f237d7316e3e87a
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2018-04-20 15:00:56 -0400

    Actual initial commit

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  FireBoss
+//
+//  Created by Jeff Kereakoglow on 4/20/18.
+//  Copyright © 2018 AlexisDigital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
