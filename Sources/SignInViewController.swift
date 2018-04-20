dc0c26be54ae8042cf2003c619112bb63680a225
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2018-04-20 15:55:33 -0400

    Add some views

---
+++ Sources/SignInViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignInViewController.swift
+//  FireBoss
+//
+//  Created by Jeff Kereakoglow on 4/20/18.
+//  Copyright © 2018 AlexisDigital. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Sources/TextFieldTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextFieldTableViewCell.swift
+//  FireBoss
+//
+//  Created by Jeff Kereakoglow on 4/20/18.
+//  Copyright © 2018 AlexisDigital. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
