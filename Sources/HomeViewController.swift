41e270dbad49d8cf7285ea8c94fc45f0d0cbaa2d
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2018-04-22 20:45:44 -0400

    Create home screen

---
+++ Sources/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  FireBoss
+//
+//  Created by Jeff Kereakoglow on 4/22/18.
+//  Copyright © 2018 AlexisDigital. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    init() {
+        super.init(nibName: "HomeView", bundle: Bundle(for: HomeViewController.self))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
